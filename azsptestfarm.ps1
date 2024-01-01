3a9a012a1455ff05fa70fc81d9ee7e00a0926464
Jan Tkac <jan.tkac@KOSMC350.int.cancom.de>
2024-01-01 01:57:29 +0100

    Removing testOutput.json

--- azsptestfarm.ps1
+++ azsptestfarm.ps1
@@ -3,5 +3,5 @@ $RGName = "rg-shp-test-eus-1"
 New-AzResourceGroup $RGName -Location eastus
 $TemplateUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/azsptestfarm1.json"
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $TemplateUri
-Remove-AzResourceGroup -Name $RGName -Force
+#Remove-AzResourceGroup -Name $RGName -Force
 
