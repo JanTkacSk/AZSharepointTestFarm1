bd3c5342dac330f5b43e379d6be25a68e5998ef5
Jan Tkac <jan.tkac@cancom.de>
2024-01-02 14:31:22 +0100

    IDK

--- azsptestfarmscript.ps1
+++ azsptestfarmscript.ps1
@@ -1,6 +1,6 @@
 $storageAccName = ""
 $RGName = "rg-shp-test-eus-1"
 New-AzResourceGroup $RGName -Location eastus
-$TemplateUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/azsptestfarm1.json"
+$TemplateUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/azsptestfarm1template.json"
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $TemplateUri
 #Remove-AzResourceGroup -Name $RGName -Force
