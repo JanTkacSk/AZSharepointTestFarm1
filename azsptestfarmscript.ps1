2f30b9bd9e7221a40b72db6a2fc15e82c5c87d7a
Jan Tkac <jan.tkac@cancom.de>
2024-01-02 14:29:23 +0100

    Renaming the main template and adjusting the azsptestfarmscript.psq accordingly.

--- azsptestfarmscript.ps1
+++ azsptestfarmscript.ps1
@@ -4,4 +4,3 @@ New-AzResourceGroup $RGName -Location eastus
 $TemplateUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/azsptestfarm1.json"
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $TemplateUri
 #Remove-AzResourceGroup -Name $RGName -Force
-
