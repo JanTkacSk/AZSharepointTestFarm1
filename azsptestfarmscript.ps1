b63f5862c975637f391e4e99f20fd1c68f87dd73
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-04 23:45:39 +0100

    added variables for files and uris

--- azsptestfarmscript.ps1
+++ azsptestfarmscript.ps1
@@ -4,5 +4,10 @@ New-AzResourceGroup $RGName -Location eastus
 $TemplateUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/azsptestfarm1template.json"
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $TemplateUri
 
-New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\VSCode\AZSharepointTestFarm1\sqlservertemplate.json -TemplateParameterFile .\VSCode\AZSharepointTestFarm1\sqlserverparameters.json
+$ParameterFileUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/sqlserverparameters.json"
+$TemplateFileUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/sqlservertemplate.json"
+$TemplateFile = ".\VSCode\AZSharepointTestFarm1\sqlservertemplate.json"
+$ParameterFile = ".\VSCode\AZSharepointTestFarm1\sqlserverparameters.json"
+
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile $TemplateUri -TemplateParameterFile $ParameterFile
 #Remove-AzResourceGroup -Name $RGName -Force
