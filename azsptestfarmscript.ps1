ad7be2b7a4ef957de126c4845cab8310aec63584
Jan Tkac <jan.tkac@cancom.de>
2024-01-04 12:16:59 +0100

    added azresourcegroupdeployment for sql

--- azsptestfarmscript.ps1
+++ azsptestfarmscript.ps1
@@ -3,4 +3,6 @@ $RGName = "rg-shp-test-eus-1"
 New-AzResourceGroup $RGName -Location eastus
 $TemplateUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/azsptestfarm1template.json"
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $TemplateUri
+
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\VSCode\AZSharepointTestFarm1\sqlservertemplate.json -TemplateParameterFile .\VSCode\AZSharepointTestFarm1\sqlserverparameters.json
 #Remove-AzResourceGroup -Name $RGName -Force
