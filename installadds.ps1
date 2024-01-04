af5e8026be3ad0e90c5b568ffb1d92c74c4cb387
Jan Tkac <jan.tkac@cancom.de>
2024-01-04 10:53:00 +0100

    addin installadds

---
+++ installadds.ps1
@@ -0,0 +1,2 @@
+Add-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName 'neverhood.local' -InstallDNS -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText 'PublicPassword123@!' -Force) -Confirm:$false
