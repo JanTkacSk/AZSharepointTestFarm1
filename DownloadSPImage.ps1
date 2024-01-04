da256bc134e6903fbbc20d7d6d6a0cc002d28c20
Jan Tkac <jan.tkac@cancom.de>
2024-01-04 14:18:20 +0100

    Added the webclient

---
+++ DownloadSPImage.ps1
@@ -0,0 +1,3 @@
+Start-BitsTransfer -Source "https://download.microsoft.com/download/C/B/A/CBA01793-1C8A-4671-BE0D-38C9E5BBD0E9/officeserver.img"  -Destination C:\temp\VSCode\
+#$WebClient = New-Object System.Net.WebClient
+#$WebClient.DownloadFile("https://download.microsoft.com/download/C/B/A/CBA01793-1C8A-4671-BE0D-38C9E5BBD0E9/officeserver.img","C:\temp\VScode")  
