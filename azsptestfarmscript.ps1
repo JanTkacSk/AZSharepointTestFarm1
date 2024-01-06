5ffc6b371757b4ae590fc18a38e223aefb266629
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-06 21:05:22 +0100

    IDK

--- azsptestfarmscript.ps1
+++ azsptestfarmscript.ps1
@@ -4,12 +4,12 @@ New-AzResourceGroup $RGName -Location eastus
 $TemplateUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/azsptestfarm1template.json"
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $TemplateUri
 
-$SQLParameterFileUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/sqlserverparameters.json"
-$SQLTemplateFileUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/sqlservertemplate.json"
+#$SQLParameterFileUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/sqlserverparameters.json"
+#$SQLTemplateFileUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/sqlservertemplate.json"
 $ADMemberTemplateUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/admembertemplate.json"
 #TemplateFile = ".\VSCode\AZSharepointTestFarm1\sqlservertemplate.json"
 #$ParameterFile = ".\VSCode\AZSharepointTestFarm1\sqlserverparameters.json"
 
-New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $TemplateFileUri -TemplateParameterUri $ParameterFileUri
+#New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $TemplateFileUri -TemplateParameterUri $ParameterFileUri
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $ADMemberTemplateUri
 #Remove-AzResourceGroup -Name $RGName -Force
