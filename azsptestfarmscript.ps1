577a6a9787a8cf1f5e77976aed9add4e93ba1ced
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-07 11:12:10 +0100

    Started using local files instead of Uris

--- azsptestfarmscript.ps1
+++ azsptestfarmscript.ps1
@@ -1,17 +1,9 @@
 #$storageAccName = ""
 $RGName = "rg-shp-test-eus-1"
 New-AzResourceGroup $RGName -Location eastus
-$TemplateFileUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/azsptestfarm1template.json"
-New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $TemplateFileUri
 
-#$SQLParameterFileUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/sqlserverparameters.json"
-#$SQLTemplateFileUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/sqlservertemplate.json"
-#$ADMemberTemplateUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/admembertemplate.json"
-$ChangeIpsToStaticUri = "https://raw.githubusercontent.com/JanTkacSk/AZSharepointTestFarm1/main/changeipstostatic.json"
-#TemplateFile = ".\VSCode\AZSharepointTestFarm1\sqlservertemplate.json"
-#$ParameterFile = ".\VSCode\AZSharepointTestFarm1\sqlserverparameters.json"
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile azsptestfarm1template.json -AsJob
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile admembertemplate.json
 
-#New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $TemplateFileUri -TemplateParameterUri $ParameterFileUri
-#New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $ADMemberTemplateUri
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile changeipstostatic.json
 #Remove-AzResourceGroup -Name $RGName -Force
