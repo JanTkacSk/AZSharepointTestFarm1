44ef24a5f368f04d71b702fea6eae98020299304
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-08 13:56:05 +0100

    A bunch pf changes.

--- joinservertoadomain.ps1
+++ joinservertoadomain.ps1
@@ -1,1 +1,17 @@
-[PSCredential]::new($jtkac, $(ConvertTo-SecureString -String "mypass" -AsPlainText -Force))
+param(
+
+[parameter(Mandatory=$true)]
+[string]$UserName,
+
+[parameter(Mandatory=$true)]
+[securestring]$Password
+
+)
+
+$Credential = [PSCredential]::new($Username, $Password)
+Add-Computer -Domain testground.eu -Credential $Credential
+
+#$UserName = "Student"
+#$Password = "PublicPassword123@!"
+#$Credential = [PSCredential]::new($UserName, $(ConvertTo-SecureString -String $Password -AsPlainText -Force))
+#Add-Computer -Domain testground.eu -Credential $Credential
