85879480cff9a6f238542d3a8c78c9d7141a19d3
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-08 14:31:33 +0100

    deleted unnecessary stuff

--- azsptestfarmscript.ps1
+++ azsptestfarmscript.ps1
@@ -1,9 +1,5 @@
-#$storageAccName = ""
 $RGName = "rg-shp-test-eus-1"
 New-AzResourceGroup $RGName -Location eastus
-
-New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile azsptestfarm1template.json -AsJob
-New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile admembertemplate.json
-
-New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile changeipstostatic.json
-#Remove-AzResourceGroup -Name $RGName -Force
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\azsptestfarm1template.json
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\admembertemplate.json
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\changeipstostatic.json
