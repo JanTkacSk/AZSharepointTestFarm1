46089798ea24246f9437101115b9f5a50b5d4136
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-16 21:06:49 +0100

    commit

--- azsptestfarmscript.ps1
+++ azsptestfarmscript.ps1
@@ -1,6 +1,7 @@
 $RGName = "rg-shp-test-eus-1"
 New-AzResourceGroup $RGName -Location eastus
-New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\azsptestfarm1template.json -storageAccName "stsp20240110085348"
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\azsptestfarm1template.json
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\shpservertemplate.json -TemplateParameterFile .\shpserverparameters.json
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\sqlservertemplate.json -TemplateParameterFile .\sqlserverparameters.json
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\SetCustomDNSInVNET.json
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\admembertemplate.json
