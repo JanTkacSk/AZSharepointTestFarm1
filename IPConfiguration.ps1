35b7a7a7580e4d2a5d44b3d8631a51aa10b238fd
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-17 18:03:43 +0100

    commit

--- IPConfiguration.ps1
+++ IPConfiguration.ps1
@@ -3,8 +3,6 @@ $NetIPAddress = Get-NetIPAddress -AddressFamily IPv4 -PrefixOrigin Dhcp
 $NIAlias = $NetIPAddress.InterfaceAlias
 $DefaultGateway = (Get-NetIPConfiguration -InterfaceAlias $NIAlias).IPv4DefaultGateway.NextHop
 Remove-NetIPAddress -InterfaceAlias $NIAlias -AddressFamily IPv4 -DefaultGateway $DefaultGateway
-$IPAddress = New-NetIPAddress -InterfaceAlias $NIAlias -IPAddress $($NetIPAddress.IPAddress) -AddressFamily IPv4 -DefaultGateway $DefaultGateway -PrefixLength $($NetIPAddress.PrefixLength)
-Start-Sleep -Seconds 3
-Get-NetIPAddress -AddressFamily IPv4 -PrefixOrigin Manual
-Set-NetIPAddress -InterfaceAlias $NIAlias -IPAddress $IPAddress -PrefixLength $($NetIPAddress.PrefixLength) -PrefixOrigin Manual
+New-NetIPAddress -InterfaceAlias $NIAlias -IPAddress $($NetIPAddress.IPAddress) -AddressFamily IPv4 -DefaultGateway $DefaultGateway -PrefixLength $($NetIPAddress.PrefixLength)
+#Set-NetIPAddress -InterfaceAlias $NIAlias -IPAddress $NetIPAddress.IPAddress -PrefixLength $($NetIPAddress.PrefixLength) -PrefixOrigin Manual
 Set-DnsClientServerAddress -InterfaceAlias $NIAlias -Addresses $DNSServers
