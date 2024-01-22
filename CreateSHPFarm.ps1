8934341211280159b53261460be30e0eb09bcff0
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-22 11:19:30 +0100

    Added two new AddWebServerToAFarm.ps1 and CreateSHPFarm.ps1

---
+++ CreateSHPFarm.ps1
@@ -0,0 +1,42 @@
+#Configuration Settings
+$DatabaseServer = "vm-shp-app-t-1"
+$ConfigDatabase = "shp-t-1_config"
+$AdminContentDB = "shp-t-1_admin"
+$Passphrase = "PublicPassword123@!"
+$FarmAccountName = "tesground\Student"
+$ServerRole="ApplicationWithSearch"
+ 
+#Get the Farm Account Credentials
+$FarmAccount = Get-Credential $FarmAccountName
+$Passphrase = (ConvertTo-SecureString $Passphrase -AsPlainText -force)
+   
+#Create SharePoint Farm
+Write-Host "Creating Configuration Database and Central Admin Content Database..."
+New-SPConfigurationDatabase -DatabaseServer $DatabaseServer -DatabaseName $ConfigDatabase -AdministrationContentDatabaseName $AdminContentDB -Passphrase $Passphrase -FarmCredentials $FarmAccount -LocalServerRole $ServerRole
+ 
+$Farm = Get-SPFarm -ErrorAction SilentlyContinue -ErrorVariable err 
+if ($Farm -ne $null)
+{
+Write-Host "Installing SharePoint Resources..."
+Initialize-SPResourceSecurity
+  
+Write-Host "Installing Farm Services ..."
+Install-SPService
+  
+Write-Host "Installing SharePoint Features..."
+Install-SPFeature -AllExistingFeatures
+  
+Write-Host "Creating Central Administration..."             
+New-SPCentralAdministration -Port 2016 -WindowsAuthProvider NTLM
+   
+Write-Host "Installing Help..."
+Install-SPHelpCollection -All 
+  
+Write-Host "Installing Application Content..."
+Install-SPApplicationContent
+   
+Write-Host "SharePoint 2016 Farm Created Successfully!"
+}
+
+
+#Read more: https://www.sharepointdiary.com/2016/12/create-sharepoint-2016-farm-using-powershell.html#ixzz8PXGAwFtT
