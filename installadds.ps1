9e99f2131fe4ed2d058b27e5b8dcea1596d68b3a
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-23 13:25:45 +0100

    commit

--- installadds.ps1
+++ installadds.ps1
@@ -1,6 +1,6 @@
 Start-Transcript C:\transcript.txt
-#Add-WindowsFeature AD-Domain-Services -IncludeManagementTools
-#Install-ADDSForest -DomainName 'testground.eu' -InstallDNS -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText 'PublicPassword123@!' -Force) -Confirm:$false
+Add-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName 'testground.eu' -InstallDNS -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText 'PublicPassword123@!' -Force) -Confirm:$false
 $UserName = "testground.eu\Student"
 $Password = "PublicPassword123@!"
 $Credential = [PSCredential]::new($UserName, $(ConvertTo-SecureString -String $Password -AsPlainText -Force))
