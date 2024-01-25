d9e6ecbb681a198536ac549887f329f64be89c5e
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-25 14:04:39 +0100

    commit

--- installadds.ps1
+++ installadds.ps1
@@ -3,6 +3,8 @@ $PwdPlain = "PublicPassword123@!"
 $PwdSecString = ConvertTo-SecureString -String $PwdPlain -AsPlainText -Force
 Add-WindowsFeature AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName 'testground.eu' -InstallDNS -SafeModeAdministratorPassword $PwdSecString -Confirm:$false
+Start-Sleep -Seconds 120
 New-ADuser -Name SPAdminAcc -AccountPassword $PwdSecString -Enabled $true
 New-ADuser -Name SPServiceAcc -AccountPassword $PwdSecString -Enabled $true
 New-ADuser -Name SQLServiceAcc -AccountPassword $PwdSecString -Enabled $true
+Stop-Transcript
