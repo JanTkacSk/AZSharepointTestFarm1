3b45f0a81ac6f37c1ed2b7910ab0ce7b7ff39c56
JanTkacSK <jan.tkac.sk@gmail.com>
2024-01-25 14:52:43 +0100

    commit

--- CreateADUsers.ps1
+++ CreateADUsers.ps1
@@ -1,4 +1,11 @@
+
 Start-Transcript  C:\CreateADUsers.txt
+do {
+    Start-Sleep -Seconds 2
+}
+until (
+    (Get-Service NTDS -EA SilentlyContinue).status -eq "running" -and (Get-Service ADWS -EA SilentlyContinue).status -eq "running"
+)
 $PwdPlain = "PublicPassword123@!"
 $PwdSecString = ConvertTo-SecureString -String $PwdPlain -AsPlainText -Force
 New-ADuser -Name SPAdminAcc -AccountPassword $PwdSecString -Enabled $true
