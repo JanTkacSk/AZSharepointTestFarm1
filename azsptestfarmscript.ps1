915ac08f471b5491ed46e073d4e5c6e9bc4a676d
JanTkacSK <jan.tkac.sk@gmail.com>
2024-03-22 10:40:10 +0100

    commit

--- azsptestfarmscript.ps1
+++ azsptestfarmscript.ps1
@@ -2,13 +2,19 @@ $RGName = "rg-shp-test-eus-1"
 New-AzResourceGroup $RGName -Location eastus
 
 #Virtual networks, ip interfaces, ips, nsgs, application groups
-New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\NetworkTemplate.json
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\ArmTemplates\10.NetworkTemplate.json
 
 #Domain controller VM and an extension to install the ADDS and domain.
-New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\DCTemplate.json
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\ArmTemplates\20.DCTemplate.json
+
+#Remove VM extension InstallADDS-and-Forest
+Remove-AzVMExtension "InstallADDS-and-Forest" -VMName "vm-dc-test-1" -Force -ResourceGroupName $RGName
+
+#Add Users
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\ArmTemplates\21.CreateADUsers.json
 
 #User assigned managed identity, deployment and a new extension to create users
-New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\ExtensionTemplate.json
+New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\ArmTemplates\30.SetCustomDnsInVNET.json
 
 #Custom VNET DNS
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\SetCustomDNSInVNET.json
@@ -21,7 +27,6 @@ New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\shpserv
 
 #Deploy a Sharepoint Web Server
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\shpservertemplate.json -TemplateParameterFile .\shpwebserverparameters.json 
-
 
 #Generic Domain Member Server
 New-AZResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile .\admembertemplate.json
